35496b91dc403ccf9769121f73ccef11f7bb95fb
228Danila228 <90102559+228Danila228@users.noreply.github.com>
2022-03-14 20:52:25 +0300

    Update first_dz.c

--- first_dz.c
+++ first_dz.c
@@ -1,41 +1,46 @@
 #include <stdio.h>
 
-int main()
-{
-	int a[5] = {10, 13, 11, 20, 25};
-	int i, l;
+int main(){
+	int i, l, n;
 	int d[20];
 	for (l = 0; l < 20; l++){
- 		d[l] = 0;
- 	}
+		d[l] = 0;
+		}
+	printf("Введите число десятичных чисел: ");
+	scanf("%d", &n);
+	int a[n];
+	int s = 0;
+	n--;
+	for(;n >= 0;n--){
+		scanf("%d", &a[n]);
+		s++;
+		}
 	l = 0;
 	printf("Десятичные числа: ");
-	for(i=0;i<5;i++){
-		printf("%d ", a[i]);	
-	}
+	for(i=0;i<s;i++){
+		printf("%d ", a[i]);
+		}
 	printf("\nДвочные числа: ");
-	for(i=0;i<5;i++){
+	for(i=0;i<s;i++){
 		while(a[i] != 0){
 			if (a[i] % 2 == 0){
-				a[i] = a[i] / 2;
 				d[l] = 0;
-			}
+				}
 			else {
-				a[i] = a[i]/ 2;
 				d[l] = 1;
-			}
+				}
+			a[i] = a[i]/ 2;
 			l++;
-		}
-		l = l - 1;
+			}
+		l--;
 		for(;l >= 0;l--){
 			printf("%d", d[l]);
-		}
+			}
 		printf(" ");
 		for (l = 0; l < 20; l++){
- 			d[l] = 0;
- 		}
+			d[l] = 0;
+			}
 		l = 0;
-		
 	}
 return(0);
 }
